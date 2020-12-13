eb7ca2629f548f6f93e6363476078a6169f79edf
sarfarazsajjad@gmail.com <Sarfaraz Hussain>
2020-12-14 00:52:15 +0500

    write into and read from database

--- example-1/app.py
+++ example-1/app.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
@@ -13,3 +14,24 @@ class User(Base):
 
 engine = create_engine('sqlite:///users.db', echo=True)
 Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+# add user into database
+# session = Session()
+#
+# user = User()
+# user.id = 0
+# user.username = 'Sarfaraz'
+#
+# session.add(user)
+# session.commit()
+#
+# session.close()
+
+# query database
+session = Session()
+
+users = session.query(User).all()
+for user in users:
+    print("Users with username=%s and id=%d" % (user.username, user.id))
+
+session.close()
